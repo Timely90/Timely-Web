5e154cefbf0caf85573ff31b72ec4da21878c924
woodyjacques <woodyjacques1@gmail.com>
2024-04-04 21:12:00 -0400

    Nuevos cambios

--- src/views/Servicios.tsx
+++ src/views/Servicios.tsx
@@ -5,39 +5,27 @@ import Header from "../components/header";
 import HeaderSesion from "../components/headerSesion";
 import { useNavigate } from "react-router-dom";
 
-export interface UserData {
-  name: string;
-  email: string;
-}
-
 function Servicios() {
+
+  const token = localStorage.getItem("ACCESS_TOKEN");
+  const roles = localStorage.getItem("USER_SESSION");
 
   const navigate = useNavigate();
 
-  const tokens = localStorage.getItem("ACCESS_TOKEN");
-
-  const roles = localStorage.getItem("USER_SESSION");
-  
-  if (roles) {
-    const userSession = JSON.parse(roles);
-    const rol = userSession.rol;
-
-    useEffect(() => {
-      if (rol == "estilista") {
+  useEffect(() => {
+    if (roles) {
+      const userSession = JSON.parse(roles);
+      const rol = userSession.rol;
+      if (rol === "estilista") {
         navigate("/timely-salon");
       }
-    }, [rol, navigate]);
-
-    if (rol) {
-      return null;
     }
-
-  }
+  }, [ roles, navigate]);
 
   return (
     <div className="flex flex-col min-h-screen bg-white">
       <div className="flex-grow">
-        {tokens ? <HeaderSesion /> : <Header />}
+        {token ? <HeaderSesion /> : <Header />}
         <Section
           tittle="Servicios"
           description="Bienvenido a mi colección de estilos, donde la creatividad y la experiencia se fusionan para ofrecerte una transformación única."
